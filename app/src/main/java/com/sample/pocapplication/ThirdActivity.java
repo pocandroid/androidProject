38fbf2fb59a3ac82bdab929f266e8d0b12cb0510
pocandroid <62979933+pocandroid@users.noreply.github.com>
2020-04-24 16:53:58 +0530

    Update ThirdActivity.java

--- app/src/main/java/com/sample/pocapplication/ThirdActivity.java
+++ app/src/main/java/com/sample/pocapplication/ThirdActivity.java
@@ -10,7 +10,6 @@ import android.content.Intent;
 import android.view.View;
 import android.widget.TextView;
 
-import com.github.barteksc.pdfviewer.PDFView;
 
 import java.io.File;
 import static android.content.Intent.FLAG_GRANT_READ_URI_PERMISSION;
@@ -19,18 +18,14 @@ import static android.provider.ContactsContract.Directory.PACKAGE_NAME;
 
 
 public class ThirdActivity extends AppCompatActivity {
-    private TextView txtView1;
-
+    
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_third);
 
-
-        PDFView pdfView=(PDFView)findViewById(R.id.pdfView);
-        pdfView.fromAsset("android_POC_ANDROID_ToolsPipeline-converted.pdf").load();
-
+        
 
     }
 }
